297b3e97d04d9989032c3d24302ef386f19efc5d
Andre Viallon <viallon.andre@gmail.com>
2021-11-30 19:38:12 +0100

    chore: signup and signin service

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,37 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { UsersRepository } from './users.repository';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from 'src/auth/interfaces/jwt-payload.interface';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(UsersRepository)
+    private usersRepository: UsersRepository,
+    private jwtService: JwtService,
+  ) {}
+
+  public async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.usersRepository.createUser(authCredentialsDto);
+  }
+
+  public async signIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    const { email, password } = authCredentialsDto;
+    const user = await this.usersRepository.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const payload: JwtPayload = { email };
+      const accessToken: string = this.jwtService.sign(payload);
+      return {
+        accessToken,
+      };
+    }
+
+    throw new UnauthorizedException('Incorrect credentials');
+  }
+}
